d54cd0dec7daf3c4a71759cce4c9e0a50ddf8ad9
Rick Wang <lurker2000wxc@gmail.com>
2019-01-22 14:14:53 +1100

    shell util

--- 2login-php.sh
+++ 2login-php.sh
@@ -1,1 +1,1 @@
-docker exec -it netojose-with-code_php_1 sh
+docker exec -it netjose_php_1 sh
---
+++ newapp.sh
@@ -0,0 +1,5 @@
+#composer create-project --prefer-dist laravel/lumen myapp
+
+#php artisan make:migration create_books_table --create=books
+
+
