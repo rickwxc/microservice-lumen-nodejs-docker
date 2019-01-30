f6ef43d46482c698df343760b4f9571b5f897d93
Rick Wang <lurker2000wxc@gmail.com>
2019-01-31 10:35:45 +1100

    shell update

--- shells/login.sh
+++ shells/login.sh
@@ -1,5 +1,5 @@
 #curl --data "email=test@test.com&password=123456" -H "Content-Type: application/x-www-form-urlencoded" -XPOST http://localhost:8080/login
 
 #call to the api gateway
-curl --data "email=test@test.com&password=123456" -H "Content-Type: application/x-www-form-urlencoded" -XPOST http://localhost:8081/login
+curl --data "email=test@test.com&password=123456" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: text/plain" -XPOST http://localhost:8081/login
 
